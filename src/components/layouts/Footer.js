8b7855a6e3be69e6edf14b46cb6a3de894a60e3c
Qhkm92 <replace-all>
2018-11-09 21:45:28 +0900

    Add Navbar, Footer, and Landing Page Component

---
+++ src/components/layouts/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Component = () => {
+    return (
+        <footer className="bg-dark text-white mg-5 p-4 text-center">
+			Copyright &copy; {new Date().getFullYear()} DevCon
+        </footer>
+    );
+};
+
+export default Component;
+	
