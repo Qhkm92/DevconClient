7b8997dfea6cc66cf63c978ce4bc964cb4be5402
Qhkm92 <replace-all>
2018-11-17 00:23:31 +0900

    add CreateProfile  component & route

---
+++ src/components/create-profile/CreateProfile.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import { Link } from 'react-router-dom';
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import TextFieldGroup from '../common/TextFieldGroup';
+
+class CreateProfile extends Component {
+	constructor(props) {
+	  super(props);
+	
+	  this.state = {
+	  	displaySocialInputs: false,
+	  	handle: '',
+	  	company: '',
+	  	website: '',
+	  	location: '',
+	  	status: '',
+	  	skills: '',
+	  	githubusername: '',
+	  	bio: '',
+	  	twitter: '',
+	  	facebook: '',
+	  	linkedin: '',
+	  	youtube: '',
+	  	instagram: '',
+	  	errors: {}
+	  };
+	}
+		render(){
+		return (
+			<div className="create-profile">
+				<div className="container">
+					<div className="row">
+						<div className="col-mid-8">
+							<div className="display-4 text-center">Create Your Profile</div>
+							<p className="lead text-center">
+								Lets get some information to make your profile stand out
+							</p>
+							<small className="d-block pb-3"> * = required filed</small>
+						</div>
+					</div>
+				</div>		  
+			</div>
+		);
+	}
+}
+
+CreateProfile.propTypes = {
+  errors: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) => ({
+	profile: state.profile,
+	errors: state.errors
+});
+
+export default connect(
+	mapStateToProps
+)(CreateProfile);
