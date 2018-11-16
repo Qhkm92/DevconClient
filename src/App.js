0db037acce1642ffb99e21e511a0e12d6c07768d
Qhkm92 <replace-all>
2018-11-16 15:04:12 +0900

    Add get current profile actions and reducer, bring in profile state when login, clear profile state when logout

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
 import { setCurrentUser, logoutUser } from './actions/authActions';
+import { clearCurrentProfile } from './actions/profileActions';
 import { Provider } from "react-redux";
 import store from "./store";
 
@@ -12,6 +13,7 @@ import Footer from "./components/layouts/Footer";
 import Landing from "./components/layouts/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import Dashboard from './components/dashboard/Dashboard';
 
 // Check for token
 if(localStorage.jwtToken) {
@@ -27,7 +29,7 @@ if(localStorage.jwtToken) {
     //Logout user
     store.dispatch(logoutUser());
     // TODO : Clear current profile
-    
+    store.dispatch(clearCurrentProfile());
     //Redirecte to login
     window.location.href = 'login';
   }
@@ -43,6 +45,7 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Route exact path="/dashboard" component={Dashboard} />
             </div>
             <Footer />
           </div>
